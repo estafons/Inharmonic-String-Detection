c7b1bb94210c39b0904196873742bffd2fe64792
stefanos koutoupis <stefanoskoutoupis@gmail.com>
2021-09-02 17:30:55 +0300

    initial commit

---
+++ track_class.py
@@ -0,0 +1,15 @@
+
+
+class TrackInstance():
+    def __init__(self):
+        pass
+
+class Tablature():
+    def __init__(self):
+        pass
+
+class NoteInstance():
+    def __init__(self):
+        pass
+    def ComputeInharmonicity(self):
+        pass
