e4b43966ec650e493b4f3c56cc784ba3152b5801
GrigorisBastas <greg.bastas@gmail.com>
2021-09-19 21:40:08 +0300

    some changes on n_tonos + first ga results.

--- exps/GuitarSet/n_tonos.py
+++ exps/GuitarSet/n_tonos.py
@@ -28,80 +28,21 @@ import statistics
 import warnings
 warnings.filterwarnings("ignore")
 
-#config_path = Path("C:\\Users/stefa/Documents//Inharmonic String Detection/exps/constants.ini")
+from GuitarSetTest import load_data, predictTabThesis, 
+
 parser = argparse.ArgumentParser()
 parser.add_argument('config_path', type=str)
 parser.add_argument('workspace_folder', type=str)
-parser.add_argument('-plot', action='store_true') 
-parser.add_argument('-run_genetic_alg', action='store_true') 
-parser.add_argument('--dataset', type=str, default= '')
-parser.add_argument('--train_mode', type=str, default= '')
-
 args = parser.parse_args()
 
 try:
     constants = Constants(args.config_path, args.workspace_folder)
 except Exception as e:
     print(e)
-
-# HARDWIRE CONSTANTS
-constants.plot = args.plot
-constants.run_genetic_alg = args.run_genetic_alg
-if args.dataset:
-    constants.dataset = args.dataset
-if args.train_mode:
-    constants.train_mode = args.train_mode
+ 
 
 
-def read_tablature_from_GuitarSet(jam_name, constants):
-    """function to read a jam file and return the annotations needed"""
-    string = 0
-    with open(jam_name) as fp:
-        try:
-            jam = jams.load(fp)
-        except:
-            print('failed again!!!!!!!!!')
-    tups = []
-    annos = jam.search(namespace='note_midi')
-    if len(annos) == 0:
-        annos = jam.search(namespace='pitch_midi')
-    for string_tran in annos:
-        for note in string_tran:
-            onset = note[0]
-            midi_note = note[2]
-            fundamental = Inharmonic_Detector.midi_to_hz(midi_note)
-            # fret = int(round(midi_note - constants.tuning[string]))
-            tups.append((onset, fundamental, string))
-        string += 1
-    tups.sort(key=lambda x: x[0])
-    return Annotations(tups, constants)
-
-def load_data(track_name, annotation_name, constants : Constants):
-    """function that loads annotation and audio file and returns instances"""
-    track_name = Path(constants.track_path + track_name)
-    annotation_name = Path(constants.annos_path + annotation_name)
-
-    data, _ = librosa.load(track_name, constants.sampling_rate) # _ cause dont need to reassign sampling rate
-    annotations = read_tablature_from_GuitarSet(annotation_name, constants)
-    tups = [(x.onset,x.fundamental, 6) for x in annotations.tablature.tablature]
-    tablature = Tablature(tups, data, constants)
-    track_instance = TrackInstance(tablature, data, constants)
-    return track_instance, annotations
-
-def predictTabThesis(track_instance : TrackInstance, annotations : Annotations, constants : Constants, StrBetaObj, filename=None):
-    global betas
-    global median_betas
-    """Inharmonic prediction of tablature as implemented for thesis """
-    for tab_instance, annos_instance in zip(track_instance.tablature.tablature, annotations.tablature.tablature):
-        ToolBoxObj = ToolBox(partial_tracking_func=compute_partials, inharmonicity_compute_func=compute_inharmonicity, partial_func_args=[constants.no_of_partials, tab_instance.fundamental/2, constants, StrBetaObj], inharmonic_func_args=[])
-        note_instance = NoteInstance(tab_instance.fundamental, tab_instance.onset, tab_instance.note_audio, ToolBoxObj, track_instance.sampling_rate, constants)
-        Inharmonic_Detector.DetectString(note_instance, StrBetaObj, constants.betafunc, constants)
-        if note_instance.string!= 6:
-            tab_instance.fret = Inharmonic_Detector.hz_to_midi(note_instance.fundamental) - constants.tuning[note_instance.string]
-            betas[note_instance.string,tab_instance.fret].append(note_instance.beta)   
-
-
-def testGuitarSet(constants : Constants, StrBetaObj):
+def compute_all_betas(constants : Constants, StrBetaObj):
     """ function that runs tests on the jams files mentioned in the given file 
     and plots the confusion matrixes for both the genetic and inharmonic results."""
 
@@ -135,16 +76,13 @@ if __name__ == '__main__':
 
     StrBetaObj = GuitarSetTrainWrapper(constants)
     # compute_partial_orders(StrBetaObj, constants)
-    testGuitarSet(constants, StrBetaObj)
+    compute_all_betas(constants, StrBetaObj)
 
     for s in range(6):
         for n in range(20):
             if betas[s,n]: # not None
-                # print(betas[s,n], statistics.median(betas[s,n]))
                 median_betas[s,n] = statistics.median(betas[s,n])
-                # print(median_betas[s,n])
 
-    # print(median_betas)
     for s in range(6):
         plt.plot(range(20), median_betas[s,:], label=str(s))
 
