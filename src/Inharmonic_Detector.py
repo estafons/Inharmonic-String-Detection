fcede7ae81d42d5115c3cdd504d2e9055fd23f84
Grigoris Bastas <greg.bastas@gmail.com>
2021-09-27 14:46:57 +0300

    just deleting old commented out block

--- src/Inharmonic_Detector.py
+++ src/Inharmonic_Detector.py
@@ -42,29 +42,6 @@ class InharmonicDetector():
     def __init__(self, NoteObj : NoteInstance, StringBetasObj : StringBetas):
         self.StringBetasObj = StringBetasObj
 
-# class GuitarSetStringBetas(StringBetas):
-#     def add_to_list(self, note_instance):
-#         self.betas_list_array[note_instance.string][note_instance.fret].append(note_instance.beta)
-#         #self.betas_list_array.remove(0)
-    
-#     def input_instance(self, instance_audio, midi_note, string, constants : Constants):
-#         fundamental = librosa.midi_to_hz(midi_note)
-#         ToolBoxObj = ToolBox(compute_partials, compute_inharmonicity, [constants.no_of_partials, fundamental/2, constants], [])
-#         note_instance = NoteInstance(fundamental, 0, instance_audio, ToolBoxObj, constants.sampling_rate, constants, midi_flag = True)
-#         fundamental = note_instance.recompute_fundamental(constants)
-#         note_instance = NoteInstance(fundamental, 0, instance_audio, ToolBoxObj, constants.sampling_rate, constants) # compute again with recomputed fundamental
-#         ToolBoxObj = ToolBox(compute_partials, compute_inharmonicity, [constants.no_of_partials, fundamental/2, constants], [])
-#         note_instance.string = string
-#         note_instance.fret = midi_note - constants.tuning[note_instance.string]
-#         return note_instance
-    
-#     def list_to_medians(self):
-#         for i, r in enumerate(self.betas_list_array):
-#             for j, l in enumerate(r):
-#                 self.betas_array[i][j] = np.median(l)
-
-
-
 
 def DetectString(NoteObj : NoteInstance, StringBetasObj : StringBetas, betafunc, constants : Constants):
     """ betafunc is the function to simulate beta. As input takes the combination and the beta array."""
@@ -74,6 +51,7 @@ def DetectString(NoteObj : NoteInstance, StringBetasObj : StringBetas, betafunc,
         NoteObj.string = min(betas, key = lambda a: a[0])[1][0] # returns comb where 0 arguement is string
     else:
         NoteObj.string = 6
+
 def hz_to_midi(fundamental):
     return round(12*math.log(fundamental/440,2)+69)
 
